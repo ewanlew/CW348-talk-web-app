039f0655415369a49f9dd62bfa52f891c16d675f
ewanlew <ewann.lew@gmail.com>
2024-11-02 16:04:20 +0000

    forgot ":" oopsies

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,11 +8,11 @@ class Post extends Model
 {
     public function user()
     {
-        return $this->belongsTo(User:class);
+        return $this->belongsTo(User::class);
     }
 
     public function comments()
     {
-        return $this->hasMany(Comment:class);
+        return $this->hasMany(Comment::class);
     }
 }
