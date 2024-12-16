9de5b474a022dd8a45974bce0e44cc7b48e6388f
ewanlew <ewann.lew@gmail.com>
2024-12-16 21:35:42 +0000

    DATABASE SEEDER FINALLY WORKS

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,17 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Post;
 use App\Models\Comment;
-use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run()
     {
-        // Seed users, each with 3 posts and each post with 2 comments
         User::factory(10)->create()->each(function ($user) {
-            $posts = Post::factory(3)->create(['user_id' => $user->id]);
-            $posts->each(function ($post) {
+            Post::factory(3)->create(['user_id' => $user->id])->each(function ($post) {
                 Comment::factory(2)->create([
                     'post_id' => $post->id,
                     'user_id' => $post->user_id,
