f3ff3912ec608f2e6bc8db98b7d5bef5a48fd74c
ewanlew <ewann.lew@gmail.com>
2024-12-16 21:40:53 +0000

    Links for viewing db

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/users', function () {
+    return User::all();
+});
+
+Route::get('/posts', function () {
+    return Post::all();
+});
+
+Route::get('/comments', function () {
+    return Comment::all();
 });
