ce0b35ef2f02a1176466d125d670e284e1cb5956
ewanlew <ewann.lew@gmail.com>
2024-12-17 23:19:16 +0000

    hyperlink on posts to go to profile

--- resources/views/posts/_post.blade.php
+++ resources/views/posts/_post.blade.php
@@ -1,6 +1,6 @@
 <div class="post">
-    <div class="post-header">
-        <span class="post-author">{{ $post->user->name }}</span>
+<div class="post-header">
+        <a href="{{ route('user.show', $post->user->id) }}" class="post-author">{{ $post->user->name }}</a>
         <span class="post-time">{{ $post->created_at->diffForHumans() }}</span>
     </div>
     <p><b>{{ $post->title }}</b></p>
